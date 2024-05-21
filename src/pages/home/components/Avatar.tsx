02ff7d672228ed80839e2374edd9eccc4a9a13d1
Klev12 <kevinsibri2003@hotmail.com>
2024-05-21 15:10:20 -0500

    avatar config

--- src/pages/home/components/Avatar.tsx
+++ src/pages/home/components/Avatar.tsx
@@ -1,6 +1,12 @@
 import { Avatar } from "primereact/avatar";
+import { OverlayPanel } from "primereact/overlaypanel";
+import { Button } from "primereact/button";
+import { OverlayPanel as OverlayPanelType } from "primereact/overlaypanel";
+import { useRef } from "react";
 
 export default function AvatarDemo() {
+  const op = useRef<OverlayPanelType>(null);
+
   return (
     <div className="card">
       <div className="flex flex-wrap gap-5">
@@ -10,7 +16,23 @@ export default function AvatarDemo() {
             className="mr-2"
             size="normal"
             shape="circle"
+            onClick={(e) => op.current?.toggle(e)}
+            style={{ cursor: "pointer" }}
           />
+          <OverlayPanel ref={op} dismissable>
+            <div className="p-3">
+              <Button
+                label="Ver Perfil"
+                className="p-button-text"
+                onClick={() => console.log("Ver Perfil")}
+              />
+              <Button
+                label="Cerrar Sesión"
+                className="p-button-text"
+                onClick={() => console.log("Cerrar Sesión")}
+              />
+            </div>
+          </OverlayPanel>
         </div>
       </div>
     </div>
