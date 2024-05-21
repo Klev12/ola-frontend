de8061a1c6992dbfff656b28f17bf8f08aab63f9
Klev12 <kevinsibri2003@hotmail.com>
2024-05-21 14:00:10 -0500

    contact

--- src/pages/home/pages/Contact.tsx
+++ src/pages/home/pages/Contact.tsx
@@ -3,14 +3,25 @@ import MenuDemo from "../components/Menu";
 export const Contact = () => {
   return (
     <div>
-      <MenuDemo />
-      <a href="https://www.instagram.com/olapublicidad.ec/" target="_blank">
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          backgroundColor: "#333",
+          padding: "10px 20px",
+        }}
+      >
+        <MenuDemo />
+      </div>
+      <a href="https://www.instagram.com/olapublicidad.ec/" target=" ">
         <i className="pi pi-instagram"></i>
       </a>
-      <a href="https://www.facebook.com/olapublicidad.ec" target="_blank">
+      <a href="https://www.facebook.com/olapublicidad.ec" target=" ">
         <i className="pi pi-facebook"></i>
       </a>
-      <a href="https://www.tiktok.com/@olapublicidad" target="_pepep">
+      <a href="https://www.tiktok.com/@olapublicidad" target=" ">
         <i className="pi pi-tiktok"></i>
       </a>
     </div>
