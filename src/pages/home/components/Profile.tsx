00ca50a6496cc67fa519ec41d3bbfafdc462cdb8
Klev12 <kevinsibri2003@hotmail.com>
2024-07-02 17:03:48 -0500

    modificate styles in menu, and update profiele

---
+++ src/pages/home/components/Profile.tsx
@@ -0,0 +1,16 @@
+import { Card } from "primereact/card";
+import useGlobalState from "../../../store/store";
+
+export const Profile = () => {
+  const user = useGlobalState((state) => state.user);
+  return (
+    <div>
+      <Card>
+        <div>Usuario: {user?.fullname}.</div>
+        <div>Correo: {user?.email}</div>
+        <div>Rol: {user?.role}.</div>
+        <div>Área: {user?.area}.</div>
+      </Card>
+    </div>
+  );
+};
