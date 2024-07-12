b271eab4704164d1aea16cc90a3a90f02581e716
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2024-07-12 16:41:07 -0500

    Add Sales changes

--- src/pages/sales-form/Payment.tsx
+++ src/pages/sales-form/Payment.tsx
@@ -1,20 +1,43 @@
 import { useContext } from "react";
 import { SalesFormContext } from "./components/WrapperSalesForm";
 import { ProgressSpinner } from "primereact/progressspinner";
+import { Button } from "primereact/button";
+import { useMutation } from "react-query";
+import { markFormToDone } from "../../services/forms-service";
+import { useParams } from "react-router";
 
 const Payment = () => {
-  const { errorMessage, isFormLoading } = useContext(SalesFormContext);
+  const { isFormLoading } = useContext(SalesFormContext);
+  const { form } = useContext(SalesFormContext);
+  const { hash } = useParams();
+
+  const { mutate: markFormToDoneMutate } = useMutation(markFormToDone, {
+    onSuccess: () => {
+      window.location.reload();
+    },
+  });
 
   if (isFormLoading) {
     return (
       <div>
         <ProgressSpinner />
-        Cargando formulario...
       </div>
     );
   }
 
-  return <div>Payment</div>;
+  return (
+    <div>
+      <Button
+        label="Pagar"
+        onClick={() => {
+          markFormToDoneMutate({
+            formId: form?.form?.id as number,
+            hash: hash as string,
+          });
+        }}
+      />
+    </div>
+  );
 };
 
 export default Payment;
