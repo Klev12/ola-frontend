fc8404e6832f513f08047362279e513e964ea50b
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2024-08-16 12:46:55 -0500

    Fix token payment

--- src/pages/sales-form/components/PayphoneButtton.tsx
+++ src/pages/sales-form/components/PayphoneButtton.tsx
@@ -24,7 +24,7 @@ const PayphoneButton = ({
           .Button({
             //token obtenido desde la consola de developer
             token:
-              "NATMcpyG4GThKdSeAZxWUnNTv54kbwq5Dbh3O26RputFUNfQ0K6Ot_FlFbuUovPPoXO4DibtqAIBIwHBocqmSofpQFD1bPQkLb9AB9sfZRAYuvpFlxtzVrtTbiG2tz5ivwlfl1uHoT_PsAbJ6K79xY3oeBwP9sIpo8bv-F6RO6qn4dr_4Hq7Nxzyk16KEXIk--zyQJQry7wRolBo9ffce4RufDSEHvhh9UFs3zl664l-AEzoi6uol52is9I3t46WaePQtfuupn-iIV0VlQAyH_kWFr2-2sBoXpOCQwSpEi08TJ_Uw59qt0imbkXc8YEjH6M1mLnhzy6qm-6WzS7OyKJWkFAk6sz9s4H49ZN5p0lfxRe008byjRJYnut2zO8_dJf5xhQ5ufZcdiIMo3H1o-78eUfkI_nrUusw_GGtDiDq97Zss0ObyYGbXtWW5zsJysXjLdVjE-dC5XJ-yw9MwoYrnnjYvNAVVA1nwVfsXPLTrZ5ARmQGDUEiN70cl5ptaOAdiGoJS2AyXZWR3weVlIES-ma5OfMMtA1X3jMw50__JTTNRMRKoF-cRR2bbFfUk55E0YFB7ZtTWx__aZWQpFUsXLUR5-2ym-zPb1QwTRVEzynKMIgQmLfh0xj5Za4OYZlap861oOSkTGaidtIjxtFX439X4AiW1LgOJr3DuIGbjRwCJZh9J4",
+              "k6sz9s4H49ZN5p0lfxRe008byjRJYnut2zO8_dJf5xhQ5ufZcdiIMo3H1o-78eUfkI_nrUusw_GGtDiDq97Zss0ObyYGbXtWW5zsJysXjLdVjE-dC5XJ-yw9MwoYrnnjYvNAVVA1nwVfsXPLTrZ5ARmQGDUEiN70cl5ptaOAdiGoJS2AyXZWR3weVlIES-ma5OfMMtA1X3jMw50__JTTNRMRKoF-cRR2bbFfUk55E0YFB7ZtTWx__aZWQpFUsXLUR5-2ym-zPb1QwTRVEzynKMIgQmLfh0xj5Za4OYZlap861oOSkTGaidtIjxtFX439X4AiW1LgOJr3DuIGbjRwCJZh9J4",
 
             //PARÁMETROS DE CONFIGURACIÓN
             btnHorizontal: true,
