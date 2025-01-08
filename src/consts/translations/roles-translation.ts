bfe2b61ade6ea6f386a9eb22bf26977fc0f539c4
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2025-01-08 14:40:33 -0500

    feat: change translatedRoles sales and colaborator

--- src/consts/translations/roles-translation.ts
+++ src/consts/translations/roles-translation.ts
@@ -2,10 +2,10 @@ import { Roles } from "../../models/user";
 
 const translatedRoles: { [key in Roles]: string } = {
   [Roles.admin]: "Administración",
-  [Roles.collaborator]: "Colaborador",
+  [Roles.sales]: "Vendedor (c/c)",
+  [Roles.collaborator]: "Vendedor (s/c)",
   [Roles.generalAdmin]: "Jefe General",
   [Roles.groupAdmin]: "Jefe de Grupo",
-  [Roles.sales]: "Vendedor",
   [Roles.secretary]: "Secretaría",
   [Roles.user]: "Usuario",
 };
