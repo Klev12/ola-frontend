a62793c8ee75530bd88b0fd02a12d4efb514dcb1
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2025-02-17 14:14:23 -0500

    feat: increment card_id heigh in sales pdf

--- src/pages/pdf/components/sales/FilesPage.tsx
+++ src/pages/pdf/components/sales/FilesPage.tsx
@@ -25,7 +25,7 @@ const FilesPage = ({ metadata }: FilesPageProps) => {
                   <img
                     src={`${ENV.BACKEND_ROUTE}/multimedia/${card.hash}`}
                     alt={card.type}
-                    height={130}
+                    height={200}
                   />
                 </div>
               );
